b5685e08da0bfa02a87f3e20ea6586d2ad3d27e8
fuckyougit <sopu55544@gmail.com>
2022-08-30 11:51:28 +0900

    디스패쳐 서블릿 직접 만들어 보기

---
+++ src/main/java/site/metacoding/dynamic/UserController.java
@@ -0,0 +1,12 @@
+package site.metacoding.dynamic;
+
+public class UserController {
+
+	public void join( ) {
+		System.out.println("join 호출됨");
+	}
+	
+	public void login() {
+		System.out.println("login 호출됨");
+	}
+}
